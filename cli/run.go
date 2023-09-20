4a11820e734c4b308b688bd652f0bc216e5d8899
DivyaReddy18 <145597037+DivyaReddy18@users.noreply.github.com>
2023-09-20 19:48:39 -0400

    Added comments for run.go
    Updating the code with comments.

--- cli/run.go
+++ cli/run.go
@@ -4,18 +4,22 @@ import (
 	argsLib "github.com/taubyte/tau-cli/cli/args"
 	"github.com/taubyte/tau-cli/i18n"
 )
-
+// Run is a function that is responsible for running the CLI application
+// It takes a variable number of string arguments
 func Run(args ...string) error {
+// Here it Creates a new instance of the CLI application
 	app, err := New()
 	if err != nil {
+// If an error occurs during app creation, it returns an error message
 		return i18n.AppCreateFailed(err)
 	}
-
+// Checks the number of arguments passed
 	if len(args) == 1 {
+// If there is only one argument, run the CLI application with that argument
 		return app.Run(args)
 	}
-
+// Parse the arguments using the app's flags and commands
 	args = argsLib.ParseArguments(app.Flags, app.Commands, args...)
-
+// Run the CLI application with the parsed arguments
 	return app.Run(args)
 }
